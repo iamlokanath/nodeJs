e355571b073c903ae3e8d669bef46903e07196aa
Lkanath Panda <lokanthpanda128@gmai.com>
2024-01-08 19:33:21 +0530

    3rd day of coding

---
+++ pathModule/path.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+console.log(path.dirname("E:/Code with Lokanath/04.NodeJs/pathModule/path.js"));
+console.log(path.extname("E:/Code with Lokanath/04.NodeJs/pathModule/path.js"));
+console.log(path.basename("E:/Code with Lokanath/04.NodeJs/pathModule/path.js"));
+
+console.log(path.parse("E:/Code with Lokanath/04.NodeJs/pathModule/path.js"));
