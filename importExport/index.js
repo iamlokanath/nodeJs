b5f3a1622e87f4b5d46746e3899ea77ad4759b47
Lkanath Panda <lokanthpanda128@gmai.com>
2024-01-08 20:03:55 +0530

    Day 3 of coding

---
+++ importExport/index.js
@@ -0,0 +1,6 @@
+// const add = require('./op');
+// console.log(add(5, 5));
+const {add , sub, multi} = require('./op');
+console.log(add(5, 5));
+console.log(sub(10, 5));
+console.log(multi(10, 5));
---
+++ importExport/op.js
@@ -0,0 +1,22 @@
+const add = (a, b) => {
+    return a + b;
+
+}
+
+const sub = (a, b) => {
+    return a - b;
+}
+
+const multi = (a, b) => {
+    return a * b;
+}
+
+// module.exports = add;
+
+
+// const name  = "Lokanath Panda";
+// module.exports.add = add;
+// module.exports.sub = sub;
+// module.exports.multi = multi;
+
+module.exports = { add, sub, multi};
