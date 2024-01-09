56a18238bbe0143e0a5065a05576d29a834cfc9f
Lkanath Panda <lokanthpanda128@gmai.com>
2024-01-09 23:32:07 +0530

    Day 4 of coding ;)

---
+++ npmMod/index.js
@@ -0,0 +1,6 @@
+// const chalk = require("chalk");
+const validator = require("validator");
+// console.log(chalk.blue.underline.inverse("Hello World"));
+const res = validator.isEmail("lokanathpanda128@gmail.com");
+console.log(res);
+
