8edd74f24751114eb6db1fd24b5e9770702399ab
Lkanath Panda <lokanthpanda128@gmai.com>
2024-01-18 23:28:29 +0530

    Day 6 of Coding routing in NOde js with http server

--- httpServer/index.js
+++ httpServer/index.js
@@ -3,10 +3,18 @@ const http = require("http");
 
 // create a http server
 
-const server = http.createServer((req, res) =>{
-    res.end("Hello From The Other Sides Lokanth");
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Hello From The Other Sides Home Page");
+  } else if (req.url === "/about") {
+    res.end("Hello From The Other Sides About Page");
+  } else if (req.url === "/contact") {
+    res.end("Hello From The Other Sides Contact Page");
+  } else {
+    res.end("404 Error Page. Pagr does not exist");
+  }
 });
 
-server.listen(8000, "127.0.0.1", () =>{
-    console.log("listening to the port no 8000");
+server.listen(8000, "127.0.0.1", () => {
+  console.log("listening to the port no 8000");
 });
