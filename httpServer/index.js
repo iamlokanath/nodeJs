559cc809e60cf4fa680492f12aebb07e13904f94
Lkanath Panda <lokanthpanda128@gmai.com>
2024-01-22 20:04:45 +0530

    Day 7 of coding How to create API

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,15 +1,26 @@
 // reqire http
 const http = require("http");
 
+const fs = require("fs");
 // create a http server
 
 const server = http.createServer((req, res) => {
+
+  const data = fs.readFileSync(`${__dirname}/UserAPI/userapi.json`, "utf-8");
+  const objData = JSON.parse(data);
+
+
   if (req.url === "/") {
     res.end("Hello From The Other Sides Home Page");
   } else if (req.url === "/about") {
     res.end("Hello From The Other Sides About Page");
   } else if (req.url === "/contact") {
     res.end("Hello From The Other Sides Contact Page");
+  } else if (req.url === "/userapi") {
+      res.writeHead(200, {"content-type": "application/json"});
+      res.end(objData[0].name);
+    
+    // res.end("Hello from the user Api page");
   } else {
     res.end("404 Error Page. Pagr does not exist");
   }
